9040ad177d9d40e3058121baa18c150bef816029
LucasAonzo <lucasaonzo@gmail.com>
2023-09-02 15:02:26 -0300

    actualizando

--- src/app.js
+++ src/app.js
@@ -35,6 +35,26 @@ server.use((req, res, next) => {
   next();
 });
 
+server.options("*", (req, res) => {
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://pifront-lucasaonzo.vercel.app",
+  ];
+
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.sendStatus(200);
+});
+
 server.use("/", routes);
 
 // Error catching endware.
